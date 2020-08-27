a851d72a4718c7110df98d94fe34079f801e6f3b
zakkiibrahiim <zakkiibrahim15@gmail.com>
2020-08-27 11:11:23 +0200

    New weeks tasks

--- src/Oppg2.java
+++ src/Oppg2.java
@@ -1,7 +1,34 @@
 public class Oppg2 {
+
+    public static int[] minmaks(int[] a)
+    {
+        int mi = 0, minverdi = a[0];
+        int ma = 0, maksverdi = a[0];
+
+        int verdi = 0;
+
+        for (int i = 1; i < a.length; i++)
+        {
+            verdi = a[i];
+
+            if (verdi > maksverdi) { maksverdi = verdi; ma = i;}
+            else if (verdi < minverdi) {minverdi = verdi; mi = i;}
+        }
+
+        return new int[]{mi,ma};
+    }
+
+
     //Oppgaver 1.1.3
-    public static void main(String[] args) {
-        int[] a = {8, 4, 17, 10, 6, 20, 1, 11, 15, 3, 18, 9, 2, 7, 19, 1};
+    public static void main(String [] args){
+
+        int[] a = {1, 8};
+
+
+        System.out.println(minmaks(a));
+
+         /*
+        int[] a = {1,8};
 
         int[] minmaks = minmaks(a);
 
@@ -47,5 +74,7 @@ public class Oppg2 {
     // Hvis n = 0 eller 1 utføres ingen multiplikasjoner.
     // HVis n > 1 utføres det n-1 multiplikasjoner.
  */
+
+    }
 
 }
